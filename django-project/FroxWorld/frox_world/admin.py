20cfef410454d90511952a6d52deff90f8bc381e
Vasiliy003 <petapetkin82@gmail.com>
2023-07-27 12:11:10 +0700

    create models

--- django-project/FroxWorld/frox_world/admin.py
+++ django-project/FroxWorld/frox_world/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Privilege)
+admin.site.register(PrivilegeType)
+admin.site.register(Purchase)
+admin.site.register(Profile)
+admin.site.register(User)
+admin.site.register(Server)
--- django-project/FroxWorld/frox_world/models.py
+++ django-project/FroxWorld/frox_world/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+class PrivilegeType(models.Model):
+    name = models.CharField(max_length=150, null=False, blank=False, default="Georgia")
+
+    def __str__(self):
+        return self.name
+
+class Privilege(models.Model):
+    price = models.IntegerField(null=False, default=0)
+    name = models.CharField(max_length=150, null=False, blank=False, default="Privilege")
+    description = models.TextField(null=False, blank=True)
+    expiration = models.DateTimeField()
+    privilege_type = models.ForeignKey(
+        'PrivilegeType', on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
+    
+class Purchase(models.Model):
+    cost = models.IntegerField(null=False, default=0)
+    date = models.DateTimeField()
+
+class Profile(models.Model):
+    image = models.ImageField()
+    skin = models.ImageField()
+
+class User(models.Model):
+    login = models.CharField(max_length=150, null=False, blank=False, default="Georgiy")
+    password = models.CharField(max_length=150, null=False, blank=False, default=123)
+    email = models.EmailField()
+    profile = models.OneToOneField(
+        'Profile', on_delete=models.CASCADE
+    )
+    
+    def __str__(self):
+        return self.login
+    
+class Server(models.Model):
+    ip = models.IntegerField(null=False)
+    name = models.CharField(max_length=150, null=False, blank=False, default="qwerty-serv")
+
+    def __str__(self):
+        return self.name
+    
