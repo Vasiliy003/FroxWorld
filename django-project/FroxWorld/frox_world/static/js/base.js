26704c1f0f6a1e37ab5e823b0083f6976fab0d11
Vasiliy003 <petapetkin82@gmail.com>
2023-10-07 18:15:10 +0700

    basket

--- django-project/FroxWorld/frox_world/static/js/base.js
+++ django-project/FroxWorld/frox_world/static/js/base.js
@@ -1,20 +1,10 @@
 let profileMenu = document.querySelector(".profile-menu");
 let burgerBtn = document.querySelector("#profile-btn");
-let navigationBtn = document.querySelector("#header-navigation-btn");
 
 function burgerMenu(event){
     profileMenu.classList.toggle("profile-menu-show")
 }
 
-function navigationDecorate(event){
-    if(event.type==="mouseover")
-        event.target.classList.toggle(".navigation-target");
-    else if(event.type==="mouseout")
-        event.target.classList.toggle(".navigation-target");
+if (burgerBtn) {
+    burgerBtn.addEventListener("click", burgerMenu)
 }
-
-
-burgerBtn.addEventListener("click", burgerMenu)
-
-navigationBtn.addEventListener("mouseover", navigationDecorate)
-navigationBtn.addEventListener("mouseout", navigationDecorate)
