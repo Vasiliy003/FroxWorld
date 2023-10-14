432294421d3a922ec02f48367fbff1d025239bf5
Vasiliy003 <petapetkin82@gmail.com>
2023-10-14 18:05:11 +0700

    c/o basket

--- django-project/FroxWorld/frox_world/static/js/donate.js
+++ django-project/FroxWorld/frox_world/static/js/donate.js
@@ -1,8 +1,43 @@
-let cartList = document.querySelector(".cart-list");
-let donateBtn = document.querySelector(".donate-cart");
+let basket = []
 
-function donateMenu(event){
-    cartList.classList.toggle("cart-list-show")
+function addToBasket(event){
+    console.log("Добавленно!")
 }
 
-donateBtn.addEventListener("click", donateMenu)
+let Privileges = document.querySelectorAll(".add-basket-btn")
+
+for(let i = 0; i < Privileges.length; i++){
+    Privileges[i].addEventListener("click", addToBasket)
+}
+
+function addToBasket(event){
+    let name = event.currentTarget.parentNode.querySelector(".donate-name").innerHTML
+    let price = event.currentTarget.parentNode.querySelector(".donate-price").innerHTML
+    let id = event.currentTarget.parentNode.parentNode.getAttribute("privilegeId")
+    let img = event.currentTarget.parentNode.parentNode.querySelector(".img-content").getAttribute("src")
+    
+    let basketObj = {
+        name: name,
+        price: price,
+        id: id,
+        img: img
+    }
+    basket.push(basketObj)
+    console.log(basket)
+
+    let number = document.querySelector(".shopping-count")
+    number.innerHTML = basket.length
+}
+
+let basketBackground = document.querySelector(".basket-background");
+let basketOpenBtn = document.querySelector(".donate-cart");
+let basketExitBtn = document.querySelector(".basket-exit-btn");
+let body = document.querySelector("#body")
+
+function openCloseBasket(event){
+    basketBackground.classList.toggle("basket-show")
+    body.classList.toggle("disable-overlow")
+}
+
+basketOpenBtn.addEventListener("click", openCloseBasket)
+basketExitBtn.addEventListener("click", openCloseBasket)
